4917f09564e05f4b6908bfb7237634068e5df728
kuya-ui <kuyamaxmillan@gmail.com>
2021-08-22 22:14:08 +0300

    add config

---
+++ config.py
@@ -0,0 +1,49 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:kuyamaxmillan@localhost/Pitches'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    #email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SENDER_EMAIL = 'kuyamaxmillan@gmail.com'
+
+
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+pass
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:kuyamaxmillan@localhost/Pitches'
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:kuyamaxmillan@localhost/Pitches'
+
+    DEBUG = True
+
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
